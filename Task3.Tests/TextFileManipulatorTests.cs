365f80929c0644bea29fd756f65d9c9c27035688
Grosss <artemenko,nickolai@gmail.com>
2017-04-17 20:26:22 +0200

    task is added

---
+++ Task3.Tests/TextFileManipulatorTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task3.Tests
+{
+    [TestFixture]
+    public class TextFileManipulatorTests
+    {
+        [TestCase("File1.txt", "yep", ExpectedResult = 3)]
+        [TestCase("File2.txt", "is", ExpectedResult = 3)]
+        public int GetWordFrequency_PassedFilePathAndWord(string filePath, string word)
+        {
+            filePath = Directory.GetCurrentDirectory() + filePath;
+            return TextFileManipulator.GetWordFrequency(filePath, word);
+        }
+    }
+}
---
+++ Task3/TextFileManipulator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Task3
+{
+    public static class TextFileManipulator
+    {
+        public static int GetWordFrequency(string filePath, string word)
+        {
+            if (ReferenceEquals(filePath, null) || ReferenceEquals(word, null))
+                throw new ArgumentNullException();
+
+            string text = File.ReadAllText(filePath).ToUpperInvariant();
+
+            return new Regex($@"(?i)\b{word}\b").Matches(text).Count;
+        }
+    }
+}
